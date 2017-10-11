7790fba1a470952a27035e757b7612e31419417a
Paul Ferguson <paul.ferguson@gmail.com>
2017-10-11 22:54:09 +0100

    Migrated to .NETStandard

--- src/FastFitParser.Tests/FastFitParserTests.cs
+++ src/FastFitParser.Tests/FastFitParserTests.cs
@@ -2,9 +2,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Text;
 
 namespace FastFitParser.Tests
 {
